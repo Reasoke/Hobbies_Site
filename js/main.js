84277707dfab095389c4cda13177abddcc4de272
Ira <ira.egoshina@gmail.com>
2023-04-17 14:05:24 +0300

    new pages and heater footer constant

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function loadFile(filePath, elementName) {
+    var request = new XMLHttpRequest();
+    request.open("GET", filePath, true);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200)
+            document.getElementById(elementName).innerHTML = request.responseText;
+    }
+    request.send(null);
+}
+
+loadFile("components/header.html", "header");
+loadFile("components/footer.html", "footer");
